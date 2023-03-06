d6060414fc575a25da20cb20232835f99595bbd2
tfermoso <tomas.fermoso@gmail.com>
2023-03-06 17:10:54 +0100

    pruebas iniciales con phpspreadsheet

---
+++ leerExcel.php
@@ -0,0 +1,10 @@
+<?php
+require 'vendor/autoload.php';
+use PhpOffice\PhpSpreadsheet\IOFactory;
+
+$spreadsheet=IOFactory::load("hello world.xlsx");
+//$worksheet=$spreadsheet->getSheetByName('Worksheet');
+$worksheet=$spreadsheet->getSheet(0);
+//$valor=$worksheet->getCell('A1')->getValue();
+$datos=$worksheet->toArray();
+var_dump($datos);
