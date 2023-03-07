7586b26e22e63c7ea6f93fbe549f3e4b361d9591
tfermoso <tomas.fermoso@gmail.com>
2023-03-07 18:44:12 +0100

    preparando importacion

--- upload.php
+++ upload.php
@@ -2,8 +2,7 @@
 require './vendor/autoload.php';
 require_once './src/excel.php';
 require_once './src/Conexion.php';
-$columnas=array();
-$productos=array();
+
 if (isset($_FILES['archivo'])) {
     $ext = strtolower(substr($_FILES['archivo']['name'], -4));
     $new_name = date("Y.m.d-H.i.s");
@@ -13,14 +12,12 @@ if (isset($_FILES['archivo'])) {
     if (!file_exists($url_insert)) {
         mkdir($url_insert, 0777, true);
     };
-    move_uploaded_file($_FILES['archivo']['tmp_name'], $dir.$new_name.'.xlsx');
-    $archivo=$dir.$new_name.'.xlsx';
-    $productos=Excel::obtieneFilas($archivo);
+    move_uploaded_file($_FILES['archivo']['tmp_name'], $dir . $new_name . '.xlsx');
+    $archivo = $dir . $new_name . '.xlsx';
+    $productos = Excel::obtieneFilas($archivo);
     //var_dump($filas_excel);
-    $conn=new Conexion();
-    $columnas=$conn->getcolumns("coches");
-
-
+    $conn = new Conexion();
+    $columnas = $conn->getcolumns("coches");
 }
 ?>
 
@@ -34,6 +31,7 @@ if (isset($_FILES['archivo'])) {
     <title>Seleccionar Archivo</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
 </head>
+
 <body>
     <div>
         <h1><strong>Subir Archivos</strong></h1>
@@ -43,26 +41,43 @@ if (isset($_FILES['archivo'])) {
         <input class="form-group" type="file" name="archivo" id="archivo">
         <button class="btn btn-primary" type="submit">Subir archivo</button>
     </form>
+    <?php
+    $contenido='
+    <div class="row">
+        <div class="col-3">
+            <input type="number" id="primera_fila" placeholder="Primera fila">
+        </div>
+        <div class="col-3">
+            <button id="btn-enviar" class="btn btn-success">Importar</button>
+        </div>
+    </div>';
+    if(isset($productos)){
+        echo $contenido;
+    }
+    ?>
     <hr>
     <table class="table table-bordered table-dark">
         <thead>
             <tr>
 
                 <?php
-                $numero_columnas=count($productos[0]);
-                for ($i = 0; $i < $numero_columnas; $i++) {
-                    echo '    <th scope="col">
+                if (isset($productos)) {
+
+
+                    $numero_columnas = count($productos[0]);
+                    for ($i = 0; $i < $numero_columnas; $i++) {
+                        echo '    <th scope="col">
                             <select class="form-control columna" id="' . $i . '">
                                 <option></option>';
 
-                    for ($j = 0; $j < count($columnas); $j++) {
-                        echo '<option>' . $columnas[$j] . '</option>';
-                    }
+                        for ($j = 0; $j < count($columnas); $j++) {
+                            echo '<option>' . $columnas[$j] . '</option>';
+                        }
 
-                    echo '     </select>
+                        echo '     </select>
                         </th>';
+                    }
                 }
-
                 ?>
 
             </tr>
@@ -70,25 +85,37 @@ if (isset($_FILES['archivo'])) {
         <tbody>
 
             <?php
+            if (isset($productos)) {
+                for ($i = 0; $i < count($productos); $i++) {
+                    echo '<tr>';
 
-            for ($i = 0; $i < count($productos); $i++) {
-                echo '<tr>';
+                    for ($j = 0; $j < $numero_columnas; $j++) {
+                        echo '  <td>' . $productos[$i][$j] . '</td>';
+                    }
 
-                for ($j = 0; $j < $numero_columnas; $j++) {
-                    echo '  <td>' . $productos[$i][$j] . '</td>';
+                    echo '</tr>';
                 }
-
-                echo '</tr>';
             }
-
-
-
             ?>
 
         </tbody>
     </table>
+    <script>
+        document.getElementById("btn-enviar").onclick = function() {
 
-       
+            var selects = document.getElementsByClassName("columna");
+
+            var columnas = [];
+            for (var i = 0; i < selects.length; i++) {
+                if (selects.item(i).value != "") {
+                    columnas.push([selects.item(i).id, selects.item(i).value]);
+                }
+            }
+            console.log(columnas);
+
+        };
+    </script>
+
 </body>
 
 </html>
