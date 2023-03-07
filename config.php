6e2257ed3669b1b8daf02d103499d8f7647637a2
tfermoso <tomas.fermoso@gmail.com>
2023-03-07 15:54:38 +0100

    pruebas

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+define("HOST","localhost");
+define("PORT",5432);
+define("DB_NAME","multibreves");
+define("DB_USER","postgres");
+define("DB_PASS","Formacom2021");
--- leerExcel.php
+++ leerExcel.php
@@ -2,7 +2,7 @@
 require 'vendor/autoload.php';
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
-$spreadsheet=IOFactory::load("hello world.xlsx");
+$spreadsheet=IOFactory::load("datos.xlsx");
 //$worksheet=$spreadsheet->getSheetByName('Worksheet');
 $worksheet=$spreadsheet->getSheet(0);
 //$valor=$worksheet->getCell('A1')->getValue();
